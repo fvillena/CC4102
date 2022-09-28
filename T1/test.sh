fbdeb26f3059f49cb5f5d9b2c5b12d9469c052ff
Fabián Villena <villenafabian@gmail.com>
2022-09-28 12:52:19 -0300

    test

---
+++ T1/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+test () {
+    algorithm=$1
+    X=$2
+    Y=$3
+    true_distance=$4
+    result=$(./measure $algorithm $X $Y)
+    result_splitted=(${result// / })
+    distance=${result_splitted[0]}
+    if [ $((distance)) = $((true_distance)) ]; then
+        echo -n "OK"
+    else
+        echo -n "FAIL"
+    fi
+}
+while IFS=" " read -r X Y true_distance
+do
+   echo "$(test 1 $X $Y $true_distance) $(test 2 $X $Y $true_distance)"
+done < test_cases.txt
