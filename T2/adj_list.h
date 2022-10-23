e90abd98c8ad09d2d34ab7b2264aff3b10483af6
Fabián Villena <villenafabian@gmail.com>
2022-10-23 15:39:11 +0000

    pretty

--- T2/adj_list.h
+++ T2/adj_list.h
@@ -12,25 +12,24 @@ void addEdge(vector<pair<int, int>> adj[], int u, int v, int w) {
   adj[v].push_back(make_pair(u, w));
 }
 
-pair <int,int> get_min(vector <int> arreglo, vector <int> auxiliar){
-
-    int tamanho=arreglo.size();
-    int minimo= INT_MAX;//INT64_MAX;
-    int indice=0;
-    for(int i=0;i<auxiliar.size();i++){
-        if(arreglo[auxiliar[i]]<=minimo){
-            minimo=arreglo[auxiliar[i]];
-            indice=auxiliar[i];
-
-        }
+pair<int, int> get_min(vector<int> arreglo, vector<int> auxiliar) {
+  int tamanho = arreglo.size();
+  int minimo = INT_MAX;  // INT64_MAX;
+  int indice = 0;
+  for (int i = 0; i < auxiliar.size(); i++) {
+    if (arreglo[auxiliar[i]] <= minimo) {
+      minimo = arreglo[auxiliar[i]];
+      indice = auxiliar[i];
     }
-    return make_pair(indice,minimo);
+  }
+  return make_pair(indice, minimo);
 }
 
-vector<vector<pair<int, int>>> make_adj_list(int n, vector<array<int,3>> edges) {
+vector<vector<pair<int, int>>> make_adj_list(int n,
+                                             vector<array<int, 3>> edges) {
   const int vertices_n = n;
   vector<pair<int, int>> adj[vertices_n];
-  for (array<int,3> edge : edges) {
+  for (array<int, 3> edge : edges) {
     addEdge(adj, edge[0], edge[1], edge[2]);
   }
   vector<vector<pair<int, int>>> v;
