51fb2b2c2a119cd1278e988a0dc9ef8d0649bd86
Fabián Villena <villenafabian@gmail.com>
2022-11-06 20:09:04 +0000

    prevent inv overflow

--- T2/dijkstra.h
+++ T2/dijkstra.h
@@ -32,7 +32,14 @@ vector<int> dijkstra_1(int n, vector<array<int, 3>> edges) {
 
     for (int i = 0; i < cant_vecinos; i++) {
       int u = grafo[index_nodo][i].first;
-      int v_u_dist = grafo[index_nodo][i].second + distancias[index_nodo];
+      int v_u_dist;
+      if (grafo[index_nodo][i].second == maximo_entero || distancias[index_nodo] == maximo_entero)
+      {
+        v_u_dist = maximo_entero;
+      } else
+      {
+        v_u_dist = grafo[index_nodo][i].second + distancias[index_nodo];
+      }
       if (v_u_dist < distancias[u]) {
         distancias[u] = v_u_dist;
         previos[u] = index_nodo;
