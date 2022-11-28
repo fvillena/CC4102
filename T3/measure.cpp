6d5ae61223122fa8337ce1e410160db5998172cf
Fabián Villena <villenafabian@gmail.com>
2022-11-28 02:02:34 +0000

    algo 2

--- T3/measure.cpp
+++ T3/measure.cpp
@@ -1,4 +1,5 @@
-#include "median.h"
+#include "median.hpp"
+#include "median2.hpp"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,9 +9,10 @@
 int main(int argc, char *argv[]) {
   int algorithm = atoi(argv[1]);
   int n = atoi(argv[2]);
-  int k = n / 2;
+  int k = n / 2; // mediana
   int c = atoi(argv[3]);
   unsigned long *A = (unsigned long*) malloc(n * sizeof(unsigned long));
+  srand((unsigned)time(NULL));
   if (strcmp(argv[4], "random") == 0) {
     for (int i = 0; i < n; i++){
       A[i] = rand();
@@ -26,11 +28,14 @@ int main(int argc, char *argv[]) {
   start = clock();
   if (algorithm == 1) {
     result = algorithm_1(A, n, k, c);
+  } else if (algorithm == 2) {
+    result = algorithm_2(A, 0, n, k, c);
   } else {
     result = -1;
   }
   end = clock();
   micros = end - start;
+  free(A);
   printf("%d %d\n", result, micros);
   return 0;
 }
